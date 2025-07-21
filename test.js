6cf66bae6ed2b2a89615a391bbc58559e3e7a270
Snow91108 <sachinrv19@gmail.com>
2025-07-21 09:09:31 +0530

    add age check function

---
+++ test.js
@@ -0,0 +1,16 @@
+let age=20;
+if(age>=18){
+    console.log("you are major")
+}else{
+    console.log("you are minor")
+}
+
+function ageCheck(age){
+    if(age>=18){
+        console.log("you are major")
+    }else{
+        console.log("you are minor")
+    }
+}
+ageCheck(20);
+ageCheck(30);
