d9bc9d2f4003614c57622370be03ae5f25fbabe5
Snow91108 <sachinrv19@gmail.com>
2025-07-21 08:48:11 +0530

    init

---
+++ Index.js
@@ -0,0 +1,18 @@
+// let person={
+//     name:'snow',
+//     age:23
+// }
+// console.log(person);
+
+//performing a task
+// function greet(firstName,lastName){
+//     console.log('hello '+firstName +' '+lastName);
+// }
+// greet('john','snow');
+
+// //calculating a value
+// function square(number){
+//     return number*number;
+// }
+// console.log(square(2))
+
